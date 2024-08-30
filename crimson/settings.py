ec45a80372ae23fc5a9f288d69261a03bfa22500
NAserge <njiafahnwiserge0000@gmail.com>
2024-08-31 00:40:25 +0100

    deploy to render

--- crimson/settings.py
+++ crimson/settings.py
@@ -23,12 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iwgcgiqdu564lil!o#8enc-+unn!ot6q$^fz=ov3@f6wxah3x_'
+# SECRET_KEY = 'django-insecure-iwgcgiqdu564lil!o#8enc-+unn!ot6q$^fz=ov3@f6wxah3x_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() =="true"
 
-ALLOWED_HOSTS = []
+
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
@@ -97,6 +102,8 @@ DATABASES = {
         
     }
 }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
